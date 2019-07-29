fdb4a1028d06f95860a133007f4aec885b1e94e4
Thai Nguyen <discoverylight@yahoo.com>
2019-07-29 07:24:43 -0700

    added file folders structure

---
+++ src/services/loginService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const client = axios.create({
+    baseURL:""
+})
+
+export default {
+    register(){
+        //to be completed
+    },
+
+    login(){
+        //to be completed
+    }
+}
