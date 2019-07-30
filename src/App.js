11f93ae507d33979050e344e2eb4bc95ea7e05f7
Suparada Saitalae <ann.suparada@gmail.com>
2019-07-30 13:39:14 -0500

    working on title Dummy Data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import registration from "./pages/registration";
 import Home from './pages/Home';
 import Login from './pages/login'
+import Dashboard from "./pages/Dashboard"
 import './App.css';
 
 
@@ -14,6 +15,7 @@ const App = () => {
       <Route exact path='/' component={Home} /> 
       <Route path="/registration" component={registration} />
       <Route path="/login" component={Login} />
+      <Route path="/Dashboard" component={Dashboard} />
     </div>
   );
 }
