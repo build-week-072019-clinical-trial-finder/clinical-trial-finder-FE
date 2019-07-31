60408de7fb4c19dd24b7379549835427c8833142
Thai Nguyen <discoverylight@yahoo.com>
2019-07-31 09:34:19 -0700

    added a Search into to Dashboard.js rearranged some stylesheets to match their components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from "react";
-import { Route } from 'react-router-dom';
-import PrivateRoute from './utilities/PrivateRoute'; 
+import { Route } from "react-router-dom";
+import PrivateRoute from "./utilities/PrivateRoute";
 import Navbar from "./components/Navbar/Navbar";
 import registration from "./pages/registration";
-import Home from './pages/Home';
-import Login from './pages/login'
-import Dashboard from "./pages/Dashboard"
-import './App.css';
+import Home from "./pages/Home";
+import Login from "./pages/login";
+import Dashboard from "./pages/Dashboard";
 
-
-import styles from "./assets/styles/theme.module.scss";
-// import "./App.css";
+import styles from "./App.module.scss";
 
 const App = () => {
   return (
-    <div className={styles.app}>
+    <div className="App">
       <Navbar />
-      <div className='App-content'>
-        <Route exact path='/' component={Home} /> 
+      <div className="App-content">
+        <Route exact path="/" component={Home} />
         <Route path="/registration" component={registration} />
         <Route path="/login" component={Login} />
         <PrivateRoute path="/Dashboard" component={Dashboard} />
       </div>
-
     </div>
   );
 };
